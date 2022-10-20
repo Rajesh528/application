ae14113132ae167f49519ff09cc86b64db262b51
vamshi <vamshivam1c@gmail.com>
2022-10-20 13:41:23 +0530

    created login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { EmailValidator } from '@angular/forms';
+import { Router } from '@angular/router';
+import { isEmpty } from 'rxjs';
 import { DataService } from '../data.service';
 
 @Component({
@@ -8,9 +11,22 @@ import { DataService } from '../data.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private dataService:DataService) { }
+  constructor( public loginData : DataService , public route_to_home:Router) {   }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+  login_info:any={email:"",password:""};
+ 
+  login(){
+   var login_status=this.loginData.VerifyData(this.login_info);
+
+console.log(this.loginData.Register);
+  if(login_status == 200){
+   console.log(login_status);
+    this.route_to_home.navigate(['home']);
   }
-
+  else{
+    alert("login failed");
+  }
+  }
 }
+  
